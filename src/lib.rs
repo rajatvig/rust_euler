000302746a93bca900d5eff806fa4d63e32c259f
Rajat Vig <rajat.vig@gmail.com>
2016-02-16 12:55:27 -0800

    allow sqrt impl

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,25 @@ pub fn primefactors(n: isize) -> Vec<isize> {
     factors
 }
 
+fn find_root(a: f64, x: f64, epsilon: f64) -> f64 {
+    let nextx = (a/x + x) / 2.0;
+    if (x-nextx).abs() < epsilon*x {
+        nextx
+    }
+    else {
+        find_root(a, nextx, epsilon)
+    }
+}
+
+pub fn sqrt(n: f64) -> f64 {
+    find_root(n, 2.0, 1.0e-40_f64)
+}
+
+#[test]
+fn test_sqrt() {
+    assert!(sqrt(4.0) == 2.0);
+}
+
 #[test]
 fn test_primefactors() {
     assert!(primefactors(10) == [2, 5]);
